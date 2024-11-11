8f5a0ac15560e7b0fd3acde06aef2616b72982be
xmmmmmovo <ma_fivezha@163.com>
2024-11-11 23:21:18 +0800

    feat: 更新第三方库

--- 3rdparty/FindFmt.cmake
+++ 3rdparty/FindFmt.cmake
@@ -2,11 +2,10 @@
 CPMAddPackage(
         NAME fmt
         GITHUB_REPOSITORY fmtlib/fmt
-        GIT_TAG 10.1.1
-        GIT_SHALLOW TRUE
+        GIT_TAG e69e5f977d458f2650bb346dadf2ad30c5320281
 )
 
 if(fmt_ADDED)
-        set_target_properties(fmt PROPERTIES CXX_STANDARD 20)
-        set_target_properties(fmt-header-only PROPERTIES CXX_STANDARD 20)
+        set_target_properties(fmt PROPERTIES CXX_STANDARD 23)
+        set_target_properties(fmt-header-only PROPERTIES CXX_STANDARD 23)
 endif()
--- 3rdparty/FindSpdlog.cmake
+++ 3rdparty/FindSpdlog.cmake
@@ -2,11 +2,13 @@
 CPMAddPackage(
         NAME spdlog
         GITHUB_REPOSITORY gabime/spdlog
-        GIT_TAG ac55e60488032b9acde8940a5de099541c4515da
+        GIT_TAG 8e5613379f5140fefb0b60412fbf1f5406e7c7f8
         OPTIONS
-        "SPDLOG_BUILD_EXAMPLE Off"
+        "SPDLOG_BUILD_EXAMPLE OFF"
         "SPDLOG_FMT_EXTERNAL ON"
         "SPDLOG_FMT_EXTERNAL_HO OFF"
+        "SPDLOG_DISABLE_DEFAULT_LOGGER ON"
+        "SPDLOG_NO_EXCEPTIONS ON"
 )
 
 if(spdlog_ADDED)
